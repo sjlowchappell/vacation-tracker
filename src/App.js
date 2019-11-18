197260ccf3b1b22399aa7b833bb866a5bebf7f2e
Sam Low-Chappell <sam@hackeryou.com>
2019-11-18 12:01:25 +0100

    playing with firebase and authentication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import firebase from './firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			user: null,
+			stops: [],
+		};
+	}
+
+	componentDidMount() {
+		auth.onAuthStateChanged(user => {
+			if (user) {
+				this.setState({ user });
+			}
+		});
+
+		const dbRef = firebase.database().ref();
+		dbRef.on('value', response => {
+			console.log(response.val());
+			const newState = [];
+			const data = response.val();
+			for (let key in data) {
+				newState.push({ key: key, name: data[key] });
+				console.log(key);
+				console.log(data[key]);
+			}
+			this.setState({
+				stops: newState,
+				userInput: '',
+			});
+		});
+	}
+
+	handleChange = e => {
+		this.setState({
+			userInput: e.target.value,
+		});
+	};
+	handleSubmit = e => {
+		e.preventDefault();
+		const dbRef = firebase.database().ref();
+		dbRef.push(this.state.userInput);
+		this.setState({ userInput: '' });
+	};
+	removeStop = stopId => {
+		const dbRef = firebase.database().ref();
+		dbRef.child(stopId).remove();
+	};
+	login = () => {
+		auth.signInWithPopup(provider).then(result => {
+			const user = result.user;
+			this.setState({
+				user,
+			});
+		});
+	};
+	logout = () => {
+		auth.signOut().then(() => {
+			this.setState({ user: null });
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<header>
+					{this.state.user ? (
+						<button onClick={this.logout}>Log Out</button>
+					) : (
+						<button onClick={this.login}>Log In</button>
+					)}
+				</header>
+				<ul>
+					{this.state.stops.map(stop => {
+						return (
+							<li>
+								<p>{stop.name}</p>
+								<button onClick={() => this.removeStop(stop.key)}>Remove Stop</button>
+							</li>
+						);
+					})}
+				</ul>
+
+				<form action="submit">
+					<label htmlFor="newStop">Add a new stop to your trip</label>
+					<input type="text" id="newStop" onChange={this.handleChange} value={this.state.userInput} />
+					<button onClick={this.handleSubmit}>Add Stop</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyCk53CdRs1UAOFWi2SyilLrXyPtFGPK10k',
+	authDomain: 'vacation-budget-tracker.firebaseapp.com',
+	databaseURL: 'https://vacation-budget-tracker.firebaseio.com',
+	projectId: 'vacation-budget-tracker',
+	storageBucket: 'vacation-budget-tracker.appspot.com',
+	messagingSenderId: '731780327006',
+	appId: '1:731780327006:web:fba006ee8750cad81f36cf',
+	measurementId: 'G-P1XHF5C878',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase;
