34a1385bcb85af7e067591d0dbf16f9a72c7813e
Sam Low-Chappell <sam@hackeryou.com>
2019-11-22 17:09:07 +0100

    added some icons to add styling to sidebar

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import sideBar from './sideBar.module.css';
+import dashboardIcon from '../assets/dashboard.svg';
+import locationIcon from '../assets/maps-and-flags.svg';
+import worldWideIcon from '../assets/worldwide.svg';
 
 const SideBar = ({ user, login, logout, stops, totalCost }) => {
 	return (
 		<div className={sideBar.container}>
 			<header>
+				<h1>Vacay Tracker!</h1>
 				{user ? (
 					<div className={sideBar.profile}>
 						<div className={sideBar.profileImageContainer}>
@@ -25,20 +29,33 @@ const SideBar = ({ user, login, logout, stops, totalCost }) => {
 					</div>
 				)}
 			</header>
-			<nav>
-				<ul>
+			<nav className={sideBar.nav}>
+				<ul className={sideBar.nav}>
 					<li>
-						<Link to="/">Dashboard</Link>
+						<Link to="/" className={sideBar.navItem}>
+							<div className={sideBar.icon}>
+								<img src={dashboardIcon} alt="" />
+							</div>
+							Dashboard
+						</Link>
 					</li>
 					<li>
-						<Link to="/stops/">Stops</Link>
+						<Link to="/stops/" className={sideBar.navItem}>
+							<div className={sideBar.icon}>
+								<img src={worldWideIcon} alt="" />
+							</div>
+							Stops
+						</Link>
 					</li>
 					<ul className={sideBar.stopsList}>
 						{stops.map(stop => {
 							// const currentStopCost = stopCost(stop.budgets);
 							return (
 								<li key={stop.key} className={sideBar.stopItem}>
-									<Link to={`/${stop.name}/`} className="stopLink">
+									<Link to={`/${stop.name}/`} className={sideBar.navItem}>
+										<div className={sideBar.icon}>
+											<img src={locationIcon} alt="" />
+										</div>
 										{stop.name}
 									</Link>
 									{/* <p className={sideBar.stopCost}>Spent: ${currentStopCost}</p> */}
