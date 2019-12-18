f68cc12449573519dfdcce83796df89d5b426dac
Sam Low-Chappell <sam@hackeryou.com>
2019-12-18 10:13:00 -0500

    updated form component

--- src/components/form.js
+++ src/components/form.js
@@ -9,37 +9,33 @@ const Form = ({ formText, inputs, handleChange, handleSubmit, submitText }) => {
 				<p>{formText}</p>
 				<div className={form.inputContainer}>
 					{inputs.map(input => {
-						if (input.type === 'select') {
-							return (
-								// Tried using uuidv4 for creating unique keys but it broke the handleChange function
-								<div key={`${input.id}Container`} className={form.inputItem}>
-									<label htmlFor="expenseCategory">Category:</label>
-									<select onChange={handleChange} name="expenseCategory" id="expenseCategory">
-										<option value="Food">Food</option>
-										<option value="Transport">Transport</option>
-										<option value="Lodging">Lodging</option>
-										<option value="Entertainment">Entertainment</option>
-										<option value="Shopping">Shopping</option>
-										<option value="Miscellaneous">Miscellaneous</option>
-									</select>
-								</div>
-							);
-						} else {
-							return (
-								// Tried using uuidv4 for creating unique keys but it broke the handleChange function
-								<div key={`${input.id}Container`} className={form.inputItem}>
-									<label htmlFor={input.id}>{input.text}: </label>
-									<input
-										type={input.type}
-										id={input.id}
-										onChange={handleChange}
-										min="0"
-										step="0.01"
-										required
-									/>
-								</div>
-							);
-						}
+						return input.type === 'select' ? (
+							// Tried using uuidv4 for creating unique keys but it broke the handleChange function
+							<div key={`${input.id}Container`} className={form.inputItem}>
+								<label htmlFor="expenseCategory">Category:</label>
+								<select onChange={handleChange} name="expenseCategory" id="expenseCategory">
+									<option value="Food">Food</option>
+									<option value="Transport">Transport</option>
+									<option value="Lodging">Lodging</option>
+									<option value="Entertainment">Entertainment</option>
+									<option value="Shopping">Shopping</option>
+									<option value="Miscellaneous">Miscellaneous</option>
+								</select>
+							</div>
+						) : (
+							// Tried using uuidv4 for creating unique keys but it broke the handleChange function
+							<div key={`${input.id}Container`} className={form.inputItem}>
+								<label htmlFor={input.id}>{input.text}: </label>
+								<input
+									type={input.type}
+									id={input.id}
+									onChange={handleChange}
+									step={input.id === 'expenseValue' ? '0.01' : null}
+									min={input.type === 'number' ? '0' : null}
+									required
+								/>
+							</div>
+						);
 					})}
 				</div>
 				<Button styleType="blue">{submitText}</Button>
