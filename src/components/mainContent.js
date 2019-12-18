0bdd1bab9a2fe17ad33724533410f48a781dc23a
Sam Low-Chappell <sam@hackeryou.com>
2019-12-18 09:43:31 -0500

    Changed stopslist to be a class component and moved handlesubmit/handlechange methods to it

--- src/components/mainContent.js
+++ src/components/mainContent.js
@@ -1,92 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import Stop from './stop';
 import mainContent from './mainContent.module.css';
 import StopsList from './stopsList';
-import firebase from '../firebase';
 import uuidv4 from 'uuid/v4';
 import inputList from '../utils/stopInputs';
 
-class MainContent extends Component {
-	constructor() {
-		super();
-		this.state = {};
-	}
-	// Method for handling form input -> sets state based on user input
-	handleChange = e => {
-		this.setState({
-			[e.target.id]: e.target.value,
-		});
-	};
-	// Method for handling form submission
-	handleSubmit = e => {
-		e.preventDefault();
-		// Get database reference for user and stops
-		const dbRef = firebase.database().ref('/users/' + this.props.uid + '/stops/');
-		// Create a new stop object with it's name, budget, arrival, departure, expenses, and cost
-		const stop = {
-			name: this.state.location,
-			budget: this.state.budget,
-			arrival: this.state.arrival,
-			departure: this.state.departure,
-			expenses: [],
-			cost: 0,
-		};
-		// Push the new stop to the database
-		dbRef.push(stop);
-		// Update state to clear out the user input
-		this.setState({ location: '', budget: 0, arrival: '', departure: '' });
-	};
-	render() {
-		const { stops, uid } = this.props;
-		return (
-			<main className={mainContent.container}>
-				{/* Set up all routes. Need to check if the Router should go here instead of on the App component */}
-				{/* Route for StopsList */}
-				<Route
-					exact
-					path={'/'}
-					render={() => {
+const MainContent = ({ stops, uid }) => {
+	return (
+		<main className={mainContent.container}>
+			{/* Set up all routes. Need to check if the Router should go here instead of on the App component */}
+			{/* Route for StopsList */}
+			<Route
+				exact
+				path={'/'}
+				render={() => {
+					return <StopsList stops={stops} uid={uid} inputList={inputList} />;
+				}}
+			/>
+			{stops.length !== 0 ? (
+				<>
+					{/* Routes for all the individual Stops */}
+					{stops.map(stop => {
 						return (
-							<StopsList
-								stops={stops}
-								uid={uid}
-								// removeStop={this.removeStop}
-								handleChange={this.handleChange}
-								handleSubmit={this.handleSubmit}
-								inputList={inputList}
+							<Route
+								key={uuidv4()}
+								path={`/${stop.name}/`}
+								render={() => {
+									return (
+										<Stop
+											name={stop.name}
+											budget={stop.budget}
+											expenses={stop.expenses}
+											stopId={stop.key}
+											cost={stop.cost}
+											uid={uid}
+										/>
+									);
+								}}
 							/>
 						);
-					}}
-				/>
-				{stops.length !== 0 ? (
-					<>
-						{/* Routes for all the individual Stops */}
-						{stops.map(stop => {
-							return (
-								<Route
-									key={uuidv4()}
-									path={`/${stop.name}/`}
-									render={() => {
-										return (
-											<Stop
-												name={stop.name}
-												budget={stop.budget}
-												expenses={stop.expenses}
-												stopId={stop.key}
-												cost={stop.cost}
-												uid={uid}
-											/>
-										);
-									}}
-								/>
-							);
-						})}
-					</>
-				) : null}
-			</main>
-		);
-	}
-}
+					})}
+				</>
+			) : null}
+		</main>
+	);
+};
 
 export default MainContent;
--- src/components/stopsList.js
+++ src/components/stopsList.js
@@ -1,38 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import stopsList from './stopsList.module.css';
+import firebase from '../firebase';
 import Form from './form';
 import StopCard from './stopCard';
 
-const StopsList = ({ inputList, stops, handleChange, handleSubmit, uid }) => {
-	return (
-		<div className={stopsList.container}>
-			<h1>Stops on your journey:</h1>
-			{stops.length !== 0 ? (
-				<>
-					<div className={stopsList.allStops}>
-						{stops.map(stop => {
-							return <StopCard stop={stop} uid={uid} />;
-						})}
-					</div>
+class StopsList extends Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+	// Method for handling form input -> sets state based on user input
+	handleChange = e => {
+		this.setState({
+			[e.target.id]: e.target.value,
+		});
+	};
+	// Method for handling form submission
+	handleSubmit = e => {
+		e.preventDefault();
+		// Get database reference for user and stops
+		const dbRef = firebase.database().ref('/users/' + this.props.uid + '/stops/');
+		// Create a new stop object with it's name, budget, arrival, departure, expenses, and cost
+		const stop = {
+			name: this.state.location,
+			budget: this.state.budget,
+			arrival: this.state.arrival,
+			departure: this.state.departure,
+			expenses: [],
+			cost: 0,
+		};
+		// Push the new stop to the database
+		dbRef.push(stop);
+		// Update state to clear out the user input
+		this.setState({ location: '', budget: 0, arrival: '', departure: '' });
+	};
+	render() {
+		const { inputList, stops, uid } = this.props;
+		return (
+			<div className={stopsList.container}>
+				<h1>Stops on your journey:</h1>
+				{stops.length !== 0 ? (
+					<>
+						<div className={stopsList.allStops}>
+							{stops.map(stop => {
+								return <StopCard stop={stop} uid={uid} />;
+							})}
+						</div>
+						<Form
+							formText="Add a new stop to your trip:"
+							inputs={inputList}
+							handleChange={this.handleChange}
+							handleSubmit={this.handleSubmit}
+							submitText={'Add Stop'}
+						/>
+					</>
+				) : (
 					<Form
 						formText="Add a new stop to your trip:"
 						inputs={inputList}
-						handleChange={handleChange}
-						handleSubmit={handleSubmit}
+						handleChange={this.handleChange}
+						handleSubmit={this.handleSubmit}
 						submitText={'Add Stop'}
 					/>
-				</>
-			) : (
-				<Form
-					formText="Add a new stop to your trip:"
-					inputs={inputList}
-					handleChange={handleChange}
-					handleSubmit={handleSubmit}
-					submitText={'Add Stop'}
-				/>
-			)}
-		</div>
-	);
-};
+				)}
+			</div>
+		);
+	}
+}
 
 export default StopsList;
