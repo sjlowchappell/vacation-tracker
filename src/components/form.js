0a12e4b0d1a1019c245748fe4727ea23a700de2e
Sam Low-Chappell <sam@hackeryou.com>
2019-12-23 09:56:25 -0500

    updated form styling

--- src/components/form.js
+++ src/components/form.js
@@ -11,9 +11,14 @@ const Form = ({ formText, inputs, handleChange, handleSubmit, submitText }) => {
 					{inputs.map(input => {
 						return input.type === 'select' ? (
 							// Tried using uuidv4 for creating unique keys but it broke the handleChange function
-							<div key={`${input.id}Container`} className={form.inputItem}>
+							<div key={`${input.id}Container`} className={form.formInput}>
 								<label htmlFor="expenseCategory">Category:</label>
-								<select onChange={handleChange} name="expenseCategory" id="expenseCategory">
+								<select
+									onChange={handleChange}
+									name="expenseCategory"
+									id="expenseCategory"
+									className={form.inputSelect}
+								>
 									<option value="Food">Food</option>
 									<option value="Transport">Transport</option>
 									<option value="Lodging">Lodging</option>
@@ -24,11 +29,12 @@ const Form = ({ formText, inputs, handleChange, handleSubmit, submitText }) => {
 							</div>
 						) : (
 							// Tried using uuidv4 for creating unique keys but it broke the handleChange function
-							<div key={`${input.id}Container`} className={form.inputItem}>
+							<div key={`${input.id}Container`} className={form.formInput}>
 								<label htmlFor={input.id}>{input.text}: </label>
 								<input
 									type={input.type}
 									id={input.id}
+									className={form.inputItem}
 									onChange={handleChange}
 									step={input.id === 'expenseValue' ? '0.01' : null}
 									min={input.type === 'number' ? '0' : null}
