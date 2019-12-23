9547a9d1708f1cd7e16e2404f90b57ece5177404
Sam Low-Chappell <sam@hackeryou.com>
2019-12-23 10:18:35 -0500

    updated login

--- src/components/login.js
+++ src/components/login.js
@@ -10,7 +10,7 @@ import moneybag from '../assets/money-bag.svg';
 const Login = ({ login }) => {
 	return (
 		<div className={styles.container}>
-			<h1>Pack-Track!</h1>
+			<h1 className={styles.heading}>Pack-Track!</h1>
 			<div className={styles.iconContainer}>
 				<div className={styles.icon}>
 					<img src={airplane} alt="SVG of an airplane" />
