578579a63e7058ce33388fe34e3ff01f775687a3
Sam Low-Chappell <sam@hackeryou.com>
2020-01-08 12:53:19 -0500

    updated firebase file so it is no longer in development build

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,6 +15,5 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
 export default firebase;
