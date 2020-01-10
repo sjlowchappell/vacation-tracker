ac1744107f211967f58fa6b1709e82cc9ce1f84f
Sam Low-Chappell <sam@hackeryou.com>
2020-01-10 09:03:41 -0500

    updated styling to make table responsive

--- src/components/stop.js
+++ src/components/stop.js
@@ -9,7 +9,7 @@ const Stop = ({ stop, uid }) => {
 	return (
 		<section className={styles.container}>
 			<div className={styles.heading}>
-				<h2>{name}</h2>
+				<h2 className={styles.headerText}>{name}</h2>
 				<h2 className={styles.budget}>Budget: ${budget}</h2>
 			</div>
 			<ExpenseList key={uuidv4()} items={expenses} stopId={key} cost={cost} budget={budget} uid={uid} />
